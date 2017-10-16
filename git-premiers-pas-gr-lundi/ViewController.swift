3aafea261207e6c7dd2d303543634fa5fa84844d
Alain <alain@LD140-PR-06284.local>
2017-10-16 09:22:42 -0400

    Ajout de la méthode afficher()

--- git-premiers-pas-gr-lundi/ViewController.swift
+++ git-premiers-pas-gr-lundi/ViewController.swift
@@ -14,13 +14,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("ViewController:viewDidLoad")
+        afficher("yé", fois: 5)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func afficher(_ msg:String, fois:Int = 1) {
+        for i in 0...fois {
+            print(i, msg)
+        } // for
+    } // afficher
 
 }
 
